11602f033a9d00b5f1a671bc57c6ddba2b17f146
Kaijapen <104542328+Kaijapen@users.noreply.github.com>
2022-09-29 13:52:45 -0700

    initial commit <3

---
+++ First_Java_Program/src/First.java
@@ -0,0 +1,11 @@
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Kaija Pendergast");
+		System.out.println("I am 20 years old");
+		System.out.println("My hometown is Novato, CA");
+
+	}
+
+}
